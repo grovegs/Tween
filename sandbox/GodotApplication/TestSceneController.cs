ae13bb3ff63afacd2578616d903779131699cf46
Gencer Güzeloğlu <59722791+GencerG@users.noreply.github.com>
2024-11-28 18:19:04 +0300

    feat: add test scene

---
+++ sandbox/GodotApplication/TestSceneController.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+using GroveGames.Tween.Context;
+using GroveGames.Tween.Easing;
+using GroveGames.Tween.TweenExtensions;
+
+using System;
+
+public partial class TestSceneController : Node3D
+{
+	[Export] Node3D _cube;
+	TweenerContext _context;
+
+    public override void _Ready()
+    {
+        _context = new TweenerContext();
+		_cube.MoveXTo(_cube.GlobalPosition.X + 2f, 5f, _context)
+		.Ease(EaseType.EaseInBack)
+		.OnComplete(() => GD.Print("Completed"))
+		.OnUpdate<float>(position => {
+			GD.Print(position);
+		});
+
+		_cube.RotateYTo(5f, 360, _context);
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_context.Update((float)delta);
+	}
+}
