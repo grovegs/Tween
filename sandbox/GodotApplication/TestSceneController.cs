035db79944f56c85081e8af1b8c0035be16887c2
Gencer Güzeloğlu <59722791+GencerG@users.noreply.github.com>
2024-12-04 12:51:04 +0300

    refactor: change method names in test scene scenario

--- sandbox/GodotApplication/TestSceneController.cs
+++ sandbox/GodotApplication/TestSceneController.cs
@@ -21,24 +21,22 @@ public partial class TestSceneController : Node3D
 		var moveRightTween = _cube.MoveXTo(_cube.GlobalPosition.X + 2f, 3f, _context);
 		moveRightTween.SetOnComplete(() => GD.Print("First Tween is Completed"));
 		moveRightTween.SetEase(EaseType.InBack);
-		sequence.Append(moveRightTween);
 
 		var rotateTween = _cube.RotateYTo(360, 4f, _context);
-		sequence.Join(rotateTween);
-
-		sequence.AppendInterval(1f);
-		sequence.AppendCallback(() => GD.Print("Callback 1 invoke"));
-
-		sequence.Join(_cube.ScaleTo(_cube.Scale * 0.25f, 1f, _context));
-
-		sequence.AppendInterval(3f);
-		sequence.AppendCallback(() => GD.Print("Callback 2 invoke"));
 
 		var moveLeftTween = _cube.MoveXTo(_cube.GlobalPosition.X, 2f, _context);
 		moveLeftTween.SetOnComplete(() => GD.Print("Second Tween is Completed"));
-		sequence.Append(moveLeftTween);
 
-		sequence.SetOnComplete(() => GD.Print("Sequence is Completed"));
+		sequence
+		.Then(moveRightTween)
+		.With(rotateTween)
+		.Wait(1f)
+		.With(_cube.ScaleTo(_cube.Scale * 0.25f, 1f, _context))
+		.Callback(() => GD.Print("Callback 1 invoke"))
+		.Wait(3f)
+		.Callback(() => GD.Print("Callback 2 invoke"))
+		.Then(moveLeftTween)
+		.SetOnComplete(() => GD.Print("Sequence is Completed"));
 	}
 
 	public override void _Process(double delta)
