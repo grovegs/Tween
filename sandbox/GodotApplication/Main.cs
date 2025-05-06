5612baf24534e747c66a22d0444dd664d8ffeaf3
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-05-06 10:37:47 +0300

    test: add object free test case

--- sandbox/GodotApplication/Main.cs
+++ sandbox/GodotApplication/Main.cs
@@ -2,7 +2,10 @@ using Godot;
 
 public partial class Main : Node2D
 {
-    public override void _Ready()
+    public override async void _Ready()
     {
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+
+        GetTree().ChangeSceneToFile("res://TestScene.tscn");
     }
 }
